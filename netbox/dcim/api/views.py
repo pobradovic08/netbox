bdfb3acfacabb88900dec8aa71eb563f5f2481a2
Arthur <worldnomad@gmail.com>
2023-04-11 11:31:47 -0700

    7503 atomic transaction

--- netbox/dcim/api/views.py
+++ netbox/dcim/api/views.py
@@ -1,5 +1,6 @@
 import socket
 
+from django.db import transaction
 from django.http import Http404, HttpResponse, HttpResponseForbidden
 from django.shortcuts import get_object_or_404
 from drf_yasg import openapi
@@ -420,6 +421,7 @@ class DeviceViewSet(ConfigContextQuerySetMixin, NetBoxModelViewSet):
 
         return serializers.DeviceWithConfigContextSerializer
 
+    @transaction.atomic
     def create(self, request, *args, **kwargs):
         # do validate / create for each item in serial instead of validating all data at once
         data_list = request.data if isinstance(request.data, list) else [request.data, ]
