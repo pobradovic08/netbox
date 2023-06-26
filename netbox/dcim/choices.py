93720b9fc964d59ab48c661bcf7b59c8f599653f
pobradovic08 <pobradovic08@gmail.com>
2023-06-27 01:05:51 +0200

    Add Molex 1x2 and 2x4

--- netbox/dcim/choices.py
+++ netbox/dcim/choices.py
@@ -402,7 +402,9 @@ class PowerPortTypeChoices(ChoiceSet):
     TYPE_NEUTRIK_POWERCON_TRUE1 = 'neutrik-powercon-true1'
     TYPE_NEUTRIK_POWERCON_TRUE1_TOP = 'neutrik-powercon-true1-top'
     TYPE_UBIQUITI_SMARTPOWER = 'ubiquiti-smartpower'
+    TYPE_MOLEX_MICRO_FIT_1X2 = 'molex-micro-fit-1x2'
     TYPE_MOLEX_MICRO_FIT_2X2 = 'molex-micro-fit-2x2'
+    TYPE_MOLEX_MICRO_FIT_2X4 = 'molex-micro-fit-2x4'
     # Other
     TYPE_HARDWIRED = 'hardwired'
     TYPE_OTHER = 'other'
@@ -520,7 +522,9 @@ class PowerPortTypeChoices(ChoiceSet):
             (TYPE_NEUTRIK_POWERCON_TRUE1, 'Neutrik powerCON TRUE1'),
             (TYPE_NEUTRIK_POWERCON_TRUE1_TOP, 'Neutrik powerCON TRUE1 TOP'),
             (TYPE_UBIQUITI_SMARTPOWER, 'Ubiquiti SmartPower'),
+            (TYPE_MOLEX_MICRO_FIT_1X2, 'Molex Micro-Fit 1x2'),
             (TYPE_MOLEX_MICRO_FIT_2X2, 'Molex Micro-Fit 2x2'),
+            (TYPE_MOLEX_MICRO_FIT_2X4, 'Molex Micro-Fit 2x4'),
         )),
         ('Other', (
             (TYPE_HARDWIRED, 'Hardwired'),
