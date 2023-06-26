72ada09c8762f338697b04256964fe4255c7e672
pobradovic08 <pobradovic08@gmail.com>
2023-06-27 00:50:21 +0200

    Add Molex Micro-Fit to PowerPortTypeChoices

--- netbox/dcim/choices.py
+++ netbox/dcim/choices.py
@@ -402,6 +402,7 @@ class PowerPortTypeChoices(ChoiceSet):
     TYPE_NEUTRIK_POWERCON_TRUE1 = 'neutrik-powercon-true1'
     TYPE_NEUTRIK_POWERCON_TRUE1_TOP = 'neutrik-powercon-true1-top'
     TYPE_UBIQUITI_SMARTPOWER = 'ubiquiti-smartpower'
+    TYPE_MOLEX_MICRO_FIT_2X2 = 'molex-micro-fit-2x2'
     # Other
     TYPE_HARDWIRED = 'hardwired'
     TYPE_OTHER = 'other'
@@ -519,6 +520,7 @@ class PowerPortTypeChoices(ChoiceSet):
             (TYPE_NEUTRIK_POWERCON_TRUE1, 'Neutrik powerCON TRUE1'),
             (TYPE_NEUTRIK_POWERCON_TRUE1_TOP, 'Neutrik powerCON TRUE1 TOP'),
             (TYPE_UBIQUITI_SMARTPOWER, 'Ubiquiti SmartPower'),
+            (TYPE_MOLEX_MICRO_FIT_2X2, 'Molex Micro-Fit 2x2'),
         )),
         ('Other', (
             (TYPE_HARDWIRED, 'Hardwired'),
